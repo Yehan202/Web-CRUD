91b3792000b79be9473b7e463c087ebcacad5940
Tu Nombre <xue200212@gmail.com>
2025-01-25 21:29:26 +0100

    123

--- data/database.py
+++ data/database.py
@@ -4,7 +4,7 @@ database = mysql.connector.connect( # LLAMAMOS AL FUNCION CONNECT PARA CONECTARN
     host ='informatica.iesquevedo.es',
     port = 3333,
     ssl_disabled = True,
-    user ='yehan.xue', #USUARIO QUE USAMOS NOSOTROS
-    password ='yehanXUE2002', #CONTRASEÑA CON LA QUE NOS CONECTAMOS
-    database='yehan'
-) 
+    user ='root', #USUARIO QUE USAMOS NOSOTROS
+    password ='1asir', #CONTRASEÑA CON LA QUE NOS CONECTAMOS
+    database='oscar'
+)  
